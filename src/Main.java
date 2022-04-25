5ca8d34864a951b148c67b3462c1ddbf4a2d644e
95kreal <95k.real@gmail.com>
2022-04-25 14:41:41 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int bonus = price / 20;
+        System.out.println("Сумма покупки: " + price + " руб.");
+        System.out.println("Ваш бонус: " + bonus + " миль");
+    }
+}
